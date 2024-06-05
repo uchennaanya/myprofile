4c946527734cf5879073f62fe06517e1e3af15c5
ucheannaanya <uchennaongit@gmail.com>
2024-06-05 15:04:45 +0100

    Fixing layout

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,38 +1,38 @@
 window.onscroll = function () {
-    scrollFunction();
-  };
+  scrollFunction();
+};
 
-  function scrollFunction() {
-    if (document.documentElement.scrollTop > 500) {
-      document.getElementById("navbar").style.padding = "1rem";
-     
-      document.getElementById("navbar").style.backgroundColor =
-        "rgba(0, 0, 0, 1)";
-      document.getElementById("arrow").style.display = "none"
-      document.querySelectorAll("a")[0].style.color =
+function scrollFunction() {
+  if (document.documentElement.scrollTop > 500) {
+    document.getElementById("navbar").style.padding = "1rem";
+
+    document.getElementById("navbar").style.backgroundColor =
+      "rgba(0, 0, 0, 1)";
+    document.getElementById("arrow").style.display = "none";
+    document.querySelectorAll("a")[0].style.color =
       document.querySelectorAll("a")[1].style.color =
       document.querySelectorAll("a")[2].style.color =
-      "#fff";
+        "#fff";
+  } else {
+    document.getElementById("navbar").style.padding = "1rem";
 
-    } else {
-      document.getElementById("navbar").style.padding = "1rem";
-      
-      document.getElementById("navbar").style.color =
-        "#000";
-      document.getElementById("arrow").style.display = "block"
-    }
+    document.getElementById("navbar").style.color = "#000";
+    document.getElementById("arrow").style.display = "block";
   }
+}
 
-  let toggle = document.getElementById("toggle")
-  let show = document.getElementById("ul")
+let toggle = document.getElementById("toggle");
+let show = document.getElementById("ul");
 
-  toggle.addEventListener("click", () => {
-    if(show.classList.contains("show")) {
-        show.classList.remove("show")
-        show.classList.add("hide")
+toggle.addEventListener("click", () => {
+  if (show.classList.contains("show")) {
+    show.classList.remove("show");
+    show.classList.add("hide");
+  } else {
+    show.classList.add("show");
+    show.classList.remove("hide");
+  }
+});
 
-    } else {
-        show.classList.add("show")
-        show.classList.remove("hide")
-    }
-  })
+let date = document.getElementById("date");
+date.innerHTML = new Date().getFullYear();
